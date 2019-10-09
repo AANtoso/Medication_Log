b5769c40e093acb3807d7562e5e4c66a7d6055db
github username <github email address>
2019-10-09 10:33:10 -0400

    Added tables to migrate files.

---
+++ db/migrate/02_create_medication.rb
@@ -0,0 +1,12 @@
+class CreateMedication < ActiveRecord::Migration
+    def change
+        create_table :medications do |t|
+            t.string :medication_name
+            t.string :class
+            t.string :indication
+            t.string :dose
+            t.string :frequency
+            t.integer :user_id
+        end
+    end
+end
