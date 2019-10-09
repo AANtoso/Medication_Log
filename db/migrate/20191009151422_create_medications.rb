66d498aa67c3ce19d57250b312f85782d5c35012
github username <github email address>
2019-10-09 11:21:03 -0400

    Created tables in migration files.

--- db/migrate/20191009151422_create_medications.rb
+++ db/migrate/20191009151422_create_medications.rb
@@ -1,4 +1,12 @@
 class CreateMedications < ActiveRecord::Migration
   def change
+    create_table :medications do |t|
+      t.string :medication_name
+      t.string :class
+      t.string :indication
+      t.string :dose
+      t.string :frequency
+      t.integer :user_id
+    end
   end
 end
