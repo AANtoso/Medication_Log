766e8cd1f0199f421a106f3ffd784d510222d7e4
github username <github email address>
2019-10-11 16:22:06 -0400

    Created routes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,36 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def logger_in?
+      !!session[user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
+  get '/signup' do
+    if !!logged_in?
+      erb :'users/new'
+    else
+      redirect "/medications"
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+        redirect "/medications"
+    else
+      erb :'users/login'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect "/"
+  end
+
+  get '/medications' do
+    if Helpers.is_logged_in?(session)
 end
