45ecfc208481a99218ccbbd485cc77f3b3b6f54a
github username <github email address>
2019-10-12 13:22:13 -0400

    Added login route.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
-    def logger_in?
-      !!session[user_id]
+    def logged_in?
+      !!session[:user_id]
     end
 
     def current_user
@@ -90,7 +90,14 @@ post 'signup' do
   end
 end
 
-
-
+post '/login' do
+  user = User.find_by(username: params[:username])
+  if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/medications'
+  else
+    redirect '/signup'
+  end
+end
 
 end
